5e9ba0203dccf2b8dd3242b7adf59971bf4dea16
Migueg04 <miguegiraldollamosa@gmail.com>
2025-06-06 15:02:49 -0500

    comentarios

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -23,17 +23,19 @@ export async function deletePostDb(postId: string) {
     await deleteDoc(docRef);
 }
 
-export async function registerUserDb(username: string, userType: string) {
+export async function registerUserDb(username: string, userType: string) { //Crear un usuario
+    // Check if the user already exists
     const userCol = collection(db, 'users');
     const docRef = await addDoc(userCol, {
         username: username,
         userType: userType
     });
-    console.log("Document written with ID: ", docRef.id);
+    console.log("Document written with ID: ", docRef.id); // Agregar un console log para verificar el ID del documento
     return docRef.id;
 }
 
-export async function getUsersDb() {
+export async function getUsersDb() { // Esta función obtiene todos los usuarios almacenados en Firestore.
+
     const usersCol = collection(db, 'users');
     const usersSnapshot = await getDocs(usersCol);
 
